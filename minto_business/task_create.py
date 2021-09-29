f3c6a610a796c35dc791369a4a777d271bc440fc
windycats <2050645108@qq.com>
2021-09-29 18:07:11 +0800

    Revert "Initial commit：2021-9-29提交代码"
    This reverts commit 9401e163

--- minto_business/task_create.py
+++
@@ -1,8 +0,0 @@
-from minto_basic.basic_element import minto_element
-
-
-class TaskCeate(minto_element):
-    def naindu_create(self, type_text, task_type, title, choosetime, member):
-        self.choose_tasktype(type_text, task_type)
-        self.click_create()
-        self.basic_info(title, choosetime, member)
